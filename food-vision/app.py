08b95fa981f4a0b24627a3ba01b3daf08f3b788b
PRATHAMSHARMA2003 <111897339+PRATHAMSHARMA2003@users.noreply.github.com>
2024-05-30 22:53:13 +0530

    Update app.py

--- food-vision/app.py
+++ food-vision/app.py
@@ -54,7 +54,7 @@ file = st.file_uploader(label="Upload an image of food.",
                         type=["jpg", "jpeg", "png"])
 
 
-model = tf.keras.models.load_model("food-vision/models/EfficientNetB1.hdf5")
+model = tf.keras.models.load_model("/food-vision/models/EfficientNetB1.hdf5")
 
 
 st.sidebar.markdown("Created by **Gaurav Reddy**")
